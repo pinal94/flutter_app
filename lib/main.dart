4a1a40def2ec358a3c3605f437edbfb51d9fb69d
pinal94 <pinal.oozee@gmail.com>
2020-01-12 18:12:40 +0530

    Navigation Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Country List',
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -55,7 +56,7 @@ class LoginState extends State<LoginPage> {
           padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
           onPressed: () {
             Navigator.push(context,
-                MaterialPageRoute(builder: (context) => SecondRoute()));
+                MaterialPageRoute(builder: (context) => NavigationActivty()));
           },
           child: Text("Login",
               textAlign: TextAlign.center,
@@ -98,21 +99,132 @@ class LoginState extends State<LoginPage> {
   }
 }
 
-class SecondRoute extends StatelessWidget {
+class NavigationActivty extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
+    return new MaterialApp(
+      title: 'Flutter Menu Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+      home: new MyNavigationPage(title: 'Dashboard'),
+    );
+  }
+}
+
+class MyNavigationPage extends StatefulWidget {
+  MyNavigationPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  MyNavigationState createState() => new MyNavigationState();
+}
+
+class MyNavigationState extends State<MyNavigationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[new Text("Dashboard")],
+        ),
+      ),
+      drawer: new Container(
+        constraints: new BoxConstraints.expand(
+            width: MediaQuery.of(context).size.width - 80),
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: new ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            new DrawerHeader(
+              padding: const EdgeInsets.all(0),
+              child: new UserAccountsDrawerHeader(
+                accountName: new Text(
+                  "Pinal Gangani",
+                  style: TextStyle(color: Colors.lightBlue),
+                ),
+                accountEmail: new Text(
+                  "pinal.gangani@gmail.com",
+                  style: TextStyle(color: Colors.lightBlue),
+                ),
+                decoration: new BoxDecoration(color: Color(0xffF5F5F5)),
+                currentAccountPicture: FlutterLogo(),
+              ),
+            ),
+            new ListTile(
+                leading: new Icon(Icons.info),
+                title: new Text("Dashboard"),
+                onTap: () {OnClickNavigation("Dashboard");}),
+            new ListTile(
+              leading: new Icon(Icons.shopping_cart),
+              title: new Text("Cart"),
+              onTap: () {OnClickNavigation("Cart");},
+            ),
+            new ListTile(
+              leading: new Icon(Icons.favorite_border),
+              title: new Text("Watch List"),
+              onTap: () {OnClickNavigation("Watch List");},
+            ),
+            new ListTile(
+              leading: new Icon(Icons.settings_power),
+              title: new Text("Logout"),
+              onTap: () {},
+            )
+          ],
         ),
       ),
     );
+  }
+
+
+  void OnClickNavigation(s) {
+    Navigator.of(context)
+        .push(new MaterialPageRoute(builder: (BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Success"),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 19),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 19),
+                      child: FlutterLogo(size: 70),
+                    ),
+                    Text(
+                      s,
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(fontWeight: FontWeight.w300),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }));
   }
 }
